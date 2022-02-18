339a9c50415eace01c5f7dde683125a2a12244ac
WinSpoon <winstone1108@naver.com>
2022-02-18 14:54:15 +0900

    Vector

---
+++ Vector/Vector_01/Main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(void)
+{
+	int Array2[11];
+	{
+		int Array[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		Array2[0] = Array[0];
+	}
+
+	Array2[0];
+
+
+
+	return  0;
+}
